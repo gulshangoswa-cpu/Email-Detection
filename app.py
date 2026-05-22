595f0130686c1dd6da8cae9f93dd8111106b13eb
VMVCUBE <vmvcube@VMVCUBEs-MacBook-Pro.local>
2026-05-22 15:57:01 +0530

    Clean __main__ block; keep top-level app

--- app.py
+++ app.py
@@ -41,7 +41,6 @@ def create_app(config_class=Config):
     return app
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000))
 
 
